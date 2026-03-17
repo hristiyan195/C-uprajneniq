c8f913e9b42e0fbfc1c1d507d014435c9cdac55e
hristiyan195 <hkarakolev@tu-sofia.bg>
2026-03-17 09:23:34 +0200

    Update upr.c

--- upr.c
+++ upr.c
@@ -2,27 +2,21 @@
 int areaofsquare(short side) {
     return side * side;
 }
-
 int areaofrectangle(short side1, short side2) {
     return side1 * side2; 
 }
-
 float areaoftriangle(short strana, short visochina) {
     return (strana * visochina) / 2.0; 
 }
-
 float areaofcircle(float radius) {
     return radius * radius * 3.1415926;
 }
-
-
 void smqnanachisla(short *chislo1, short *chislo2) {
     short temp;
     temp = *chislo1;
     *chislo1 = *chislo2;
     *chislo2 = temp;
 }
-
 int chisla(short chislo) {
     if (chislo % 2 == 0) {
         return 1;
@@ -30,14 +24,11 @@ int chisla(short chislo) {
         return 2;
     }
 }
-
-
 void decimal_to_binary(int num) {
     if (num == 0) {
         printf("0");
         return;
     }
-
     int binary_array[32];
     int index = 0;
 
@@ -46,7 +37,6 @@ void decimal_to_binary(int num) {
         num = num / 2;
         index++;
     }
-
     for (int i = index - 1; i >= 0; i--) {
         printf("%d", binary_array[i]);
     }
@@ -95,7 +85,6 @@ void task1() {
             printf("Greshen kod!\n");
     }
 }
-
 void task2() {
     short num1, num2;
     printf("\nVuvedete dve chisla (razdeleni s interval): ");
@@ -106,35 +95,26 @@ void task2() {
     
     printf("\nSLED razmqnata:\nParvo chislo: %hd\nVtoro chislo: %hd\n", num1, num2);
 }
-
 void task3() {
     short chislo1;
     printf("\nVuvedete chislo za proverka (chetno/nechetno): ");
     scanf("%hd", &chislo1);
-    
     if (chisla(chislo1) == 1) {
         printf("Chisloto e chetno\n");
     } else {
         printf("Chisloto e nechetno\n");
     }
 }
-
 void task4() {
     int number;
     printf("\nVuvedete polojitelno desetichno chislo: ");
     scanf("%d", &number);
-
     printf("Dvoichnoto predstavqne na %d e: ", number);
     decimal_to_binary(number);
     printf("\n");
 }
-
-
-
-
 int main() {
     int choice;
-    
     do {
         printf("1. Lice na geometrichni figuri\n");
         printf("2. Razmqna na dve chisla\n");
@@ -147,7 +127,6 @@ int main() {
         if (choice == 0) {
             break;
         }
-        
         switch(choice) {
             case 1: task1(); break;
             case 2: task2(); break;
@@ -155,7 +134,6 @@ int main() {
             case 4: task4(); break;
             default: printf("\nNevaliden izbor! Molq opitaite otnovo.\n");
         }
-        
     } while (choice != 0);
     
     return 0;
